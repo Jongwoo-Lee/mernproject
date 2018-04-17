2d143207ce14f4a543d4f5fc51be7bcb83f40545
Jongwoo Lee <jongwoolee2014@u.northwestern.edu>
2018-04-18 00:34:36 +0900

    middle of profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load Profile Model
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // @route   GET api/profile/test
 // @desc    Test profile route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+
+// @route   GET api/profile/
+// @desc    Get current users profile
+// @access  Private
 
 module.exports = router;
