203b4a6396407e444afb1f4c36d6a15dc1485a96
Ubuntu <ubuntu@ip-172-31-20-175.ap-northeast-2.compute.internal>
2018-06-14 01:08:24 +0000

    new branch from aws ec2

--- server.js
+++ server.js
@@ -33,14 +33,14 @@ app.use("/api/posts", posts);
 app.use("/api/profile", profile);
 
 // Server static assets if in production
-if (process.env.NODE_ENV === "production") {
+//if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
+//}
 
 const port = 5000;
 
